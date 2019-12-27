a8fbeef1004d7cad8315fdc48010d4f935e8ef8f
albaloma <59278652+albaloma@users.noreply.github.com>
2019-12-27 13:01:44 +0100

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,27 @@
-function VENTANAALERTA ()
-    {
-        alert("ATENCIÓN VENTANA DE ALERTA");
+function VentanaAlerta()
+   {
+       alert("ATENCION VENTANA DE ALERTA");
     }
-function VENTANACONFIRMACION (){
+function VentanaConfirmacion(){
     var respuesta;
-    if(confirm("Desea Confirmar?"))
-{
-respuesta= "Has pulsado OK";
-    }
-    else
-    {
-        respuesta="Has pulsado CANCEL";
-    }
-    document.getElementById("MensajeConfirmacion").innerHTML=respuesta;
-}
-function PIDEINFORMACION()
-{
+        if(confirm("¿Desea Confirmar?"))
+        {
+            respuesta= "Has pulsado OK";
+        }
+        else
+        {
+            respuesta="Has pulsado CANCEL";
+        }
+        document.getElementById("MensajeConfirmacion").innerHTML=respuesta;}
+function PideInformacion(){
     var respuesta;
-    var person= prompt("Cómo te llamas?");
-    if(person==null||person=="")
-    {
-        respuesta="El usuario pulso CANCEL";
-    }
-    else
-    {
-        respuesta= "Hola "+ person + ". ¿Que tal te va la vida?";
-    }
-    document.getElementById("MensajeInformacion").innerHTML=respuesta;
-}
+        var person=prompt("¿Cómo te llamas?");
+        if(person==null||person=="")
+        {
+            respuesta="El usuario pulsó CANCEL";
+        }
+        else
+        {
+            respuesta="Hola "+ person + ". ¿Qué tal te va la vida?";
+        }
+        document.getElementById("MensajeInformacion").innerHTML=respuesta;}
